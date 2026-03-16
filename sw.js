53ab49a374fa8317683840de5b88ea56f75f1568
mrcauliman <mrcauliman@gmail.com>
2026-03-16 00:21:08 +0000

    fix: disable stale MONOLITH service worker cache

--- sw.js
+++ sw.js
@@ -1,34 +1,9 @@
-const CACHE_NAME="monolith-cache-999999";
-const CACHE="monolith-v3-1770683485";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/rules.html",
-  "/assets/monolith_logo.webp",
-  "/assets/monolith_logo.png",
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE).then((c) => c.put(req, copy));
-      return res;
-    }).catch(() => caches.match("/")))
-  );
-});
+self.addEventListener('fetch', () => {});
